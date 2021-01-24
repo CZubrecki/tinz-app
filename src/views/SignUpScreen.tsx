35e365f76fd9fe5c2bfc9eccf38075726f7ea10a
CZubrecki <c.m.zubrecki@gmail.com>
2021-01-24 18:18:30 -0500

    Add sign up screen

--- src/views/SignUpScreen.tsx
+++ src/views/SignUpScreen.tsx
@@ -1,14 +1,127 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useContext, useEffect } from 'react';
+import { useForm } from 'react-hook-form';
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { AuthContext } from '../context/AuthContext';
 
 interface SignUpScreenProps {
     closeModal: () => void,
 }
 
 export default function SignUpScreen({ closeModal }: SignUpScreenProps) {
+    const { signUp } = useContext(AuthContext);
+    const { register, handleSubmit, setValue, getValues } = useForm();
+    const onSubmit = async (data: any) => {
+        if ((_.isNil(data.email) || _.isNil(data.password)) || (data.email.trim() === '' || data.password.trim() === '')) {
+            Alert.alert('There are missing fields');
+            return;
+        }
+
+        if (data.password.length < 8) {
+            Alert.alert('Password must be 8 or more characters long');
+            return;
+        }
+        
+        signUp(data.email, data.password);
+    }
+
+    useEffect(() => {
+        register('email');
+        register('password');
+    }, [register]);
+
     return(
-        <View>
-            
+        <View style={styles.container}>
+            <View style={styles.form}>
+                <TextInput
+                    placeholder='Email'
+                    placeholderTextColor="#BEBEBE"
+                    clearButtonMode="always"
+                    autoCapitalize="none"
+                    style={styles.input}
+                    onChangeText={text => {
+                        setValue('email', text);
+                    }}
+                />
+                <View style={styles.divider} />
+                <TextInput
+                    placeholder='Password'
+                    placeholderTextColor="#BEBEBE"
+                    clearButtonMode="always"
+                    autoCapitalize="none"
+                    style={styles.input}
+                    secureTextEntry={true}
+                    onChangeText={text => {
+                        setValue('password', text);
+                    }}
+                />
+            </View>
+            <View style={styles.buttonContainer}>
+                <View>
+                    <TouchableOpacity
+                        style={styles.signUpButton}
+                        onPress={handleSubmit(onSubmit)}>
+                        <Text style={styles.signUpText}>
+                            Sign Up
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.existingAccountContainer}>
+                    <TouchableOpacity onPress={closeModal}>
+                        <Text style={styles.existingAccountText}>
+                            Already have an account?
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    form: {
+        marginVertical: '7%',
+        width: '90%',
+        borderWidth: 1,
+        borderColor: '#BEBEBE',
+        borderRadius: 3,
+        height: 75,
+    },
+    divider: {
+        borderWidth: .5,
+        borderColor: '#BEBEBE',
+        height: 1,
+    },
+    input: {
+        height: '50%',
+        justifyContent: 'center',
+        paddingHorizontal: '3%',
+    },
+    buttonContainer: {
+        width: '90%',
+    },
+    signUpButton: {
+        backgroundColor: '#0669DD',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 37.5,
+        borderRadius: 5,
+    },
+    signUpText: {
+        fontWeight: '600',
+        fontSize: 16,
+        color: '#FFF',
+    },
+    existingAccountContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingVertical: '5%',
+    },
+    existingAccountText: {
+        fontSize: 18,
+    }
+});
